efb7b88448ea229952f5eda73e42a152c2049b5f
HarryPotter204 <soma.toyoo@gmail.com>
2025-09-02 23:49:03 +0900

    Update next.config.tsx

--- next.config.tsx
+++ next.config.tsx
@@ -1,6 +1,7 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -17,7 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // experimental.appDir は削除
 };
 
 export default nextConfig;
